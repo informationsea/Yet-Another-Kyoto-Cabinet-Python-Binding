1b09669f6e05aa7a856d9855a93cea4f5d509085
OKAMURA Yasunobu <okamura@informationsea.info>
2014-07-04 14:48:17 +0900

    add kyotocabinet sources

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from distutils.core import setup, Extension
+import os.path
+
+kyotocabinet_cc = "kcutil.cc", "kcthread.cc", "kcfile.cc", "kccompress.cc", "kccompare.cc", "kcmap.cc", "kcregex.cc", "kcdb.cc", "kcplantdb.cc", "kcprotodb.cc", "kcstashdb.cc", "kccachedb.cc", "kchashdb.cc", "kcdirdb.cc", "kctextdb.cc", "kcpolydb.cc", "kcdbext.cc", "kclangc.cc"
 
 module1 = Extension('yakc',
-                    include_dirs = ['/opt/local/include'],
-                    library_dirs = ['/opt/local/lib'],
-                    libraries = ['kyotocabinet'],
-                    sources = ['yakc.cpp'])
+                    include_dirs = ['kyotocabinet-1.2.76'],
+                    sources = ['yakc.cpp'] + [os.path.join('kyotocabinet-1.2.76', x) for x in kyotocabinet_cc])
 
 setup (name = 'yakyotocabinet',
        version = '0.1',
