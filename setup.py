26655f561e58ff06f72d2f8e7e23e5c85724f96a
OKAMURA Yasunobu <okamura@informationsea.info>
2014-09-11 20:39:37 +0900

    update version number

--- setup.py
+++ setup.py
@@ -3,14 +3,17 @@ import os.path
 
 kyotocabinet_cc = "kcutil.cc", "kcthread.cc", "kcfile.cc", "kccompress.cc", "kccompare.cc", "kcmap.cc", "kcregex.cc", "kcdb.cc", "kcplantdb.cc", "kcprotodb.cc", "kcstashdb.cc", "kccachedb.cc", "kchashdb.cc", "kcdirdb.cc", "kctextdb.cc", "kcpolydb.cc", "kcdbext.cc", "kclangc.cc"
 
+#kyotocabinet_cc = []
+
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 module1 = Extension('yakc',
                     include_dirs = ['kyotocabinet-1.2.76'],
-                    sources = ['yakc.cpp'] + [os.path.join('kyotocabinet-1.2.76', x) for x in kyotocabinet_cc])
+                    sources = ['yakc.cpp'] + [os.path.join('kyotocabinet-1.2.76', x) for x in kyotocabinet_cc],
+                    libraries=[])
 
 setup (name = 'yakc',
-       version = '0.1.4',
+       version = '0.1.5',
        description = 'Yet Another Kyoto Cabinet Binding',
        author = 'OKAMURA Yasunobu',
        author_email = 'okamura@informationsea.info',
